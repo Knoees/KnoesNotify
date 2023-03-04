1cb7613383ea24b51285da337b5e72cc09411491
Pumpkin Shop <119126465+Knoees@users.noreply.github.com>
2023-03-05 00:20:14 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Knoes#2023'
+description 'KnoesNotify'
+version '1.0'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
+
+export 'Alert'
+
+lua54 'yes'
